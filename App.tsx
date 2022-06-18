9b8c3f7ac0ef75a42f2e607cb75bb106770f51fa
Aiden Flynn <aidenflynn@MacBook-Pro.cable.rcn.com>
2022-06-18 12:20:10 -0400

    trying to work with typescript to get right props working

--- App.tsx
+++ App.tsx
@@ -1,16 +1,49 @@
-import React, { memo, useState, useCallback } from 'react'
+import React, { 
+  memo, 
+  useState, 
+  useCallback,
+  useEffect,
+  useRef
+} from 'react'
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native'
 import { useSafeAreaInsets, SafeAreaProvider } from 'react-native-safe-area-context'
-// import _ from 'lodash'
 import GridView from 'react-native-draggable-gridview'
+import axios from 'axios'
+
+interface Data {
+  id: string,
+  name: string,
+  calories: string,
+  description: string
+}
+
+interface ContainerProps {
+  newFoods: []
+}
 
 /**
  * App
  */
 export default function App() {
+  
+  ///const foods = useRef([])
+  const [foods, setFoods] = useState([])
+  useEffect(() => {
+    axios.get("http://localhost:3001/foods/get-all-foods")
+    .then(res => {
+      // foods.current = res.data.foods
+      setFoods(res.data.foods)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }, [])
+
   return (
     <SafeAreaProvider>
-      <Container />
+      {
+        foods && <Container newFoods={foods}/>
+      }
     </SafeAreaProvider>
   )
 }
@@ -18,11 +51,27 @@ export default function App() {
 /**
  * Container
  */
-const Container = memo(() => {
+const Container = memo(({ newFoods }: ContainerProps) => {
+  //debugger
+  interface Data {
+    id: string,
+    name: string,
+    calories: string,
+    description: string
+  }
+  
+  const newData = (i: number): Data => ({
+    id: uuid(),
+    name: newFoods[i].name,
+    calories: newFoods[i].calories,
+    description: newFoods[i].description
+    // id: uuid(),
+    // color: colors[i % colors.length],
+  })
   const { top, bottom } = useSafeAreaInsets()
   const [editing, setEditing] = useState(false)
   const [data, setData] = useState<Data[]>(
-    Array.from(new Array(14)).map((v, i) => newData(i))
+    Array.from(newFoods).map((v, i) => newData(i))
   )
 
   const onPressEdit = useCallback(() => {
@@ -46,6 +95,8 @@ const Container = memo(() => {
   const onBeginDragging = useCallback(() => !editing && setEditing(true), [
     editing,
   ])
+
+  /* onPressCell can only be called when not in editing mode */
 
   const onPressCell = useCallback((item) => !editing && alert(item.color), [
     editing,
@@ -92,17 +143,31 @@ const Container = memo(() => {
 /**
  * Data
  */
-const colors = ['red', 'orange', 'green', 'cyan', 'blue', 'purple', 'pink']
+//const colors = ['red', 'orange', 'green', 'cyan', 'blue', 'purple', 'pink']
+// let foods;
+// const foods = axios.get("http://localhost:3001/foods/get-all-foods")
+// .then(res => {
+//   return res.data.foods
+// })
+// .catch(err => {
+//   console.log(err)
+// })
 
-interface Data {
-  id: string
-  color?: string
-}
+// interface Data {
+//   id: string,
+//   name: string,
+//   calories: string,
+//   description: string
+// }
 
-const newData = (i: number): Data => ({
-  id: uuid(),
-  color: colors[i % colors.length],
-})
+// const newData = (i: number): Data => ({
+//   id: uuid(),
+//   name: foods[i].name,
+//   calories: foods[i].calories,
+//   description: foods[i].description
+//   // id: uuid(),
+//   // color: colors[i % colors.length],
+// })
 
 /**
  * Item
@@ -239,341 +304,3 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
   },
 })
-
-
-// /*
-//   !!! https://docs.amplify.aws/start (React Native & Expo)
-
-//   When you initialize a new Amplify project, a few things happen:
-
-//   It creates a top level directory called amplify that stores your backend definition. During the tutorial you'll add capabilities such as a GraphQL API 
-//   and authentication. As you add features, the amplify folder will grow with infrastructure-as-code templates that define your backend stack. 
-//   Infrastructure-as-code is a best practice way to create a replicable backend stack.
-
-//   It creates a file called aws-exports.js in the src directory that holds all the configuration for the services you create with Amplify. 
-//   This is how the Amplify client is able to get the necessary information about your backend services.
-
-//   It modifies the .gitignore file, adding some generated files to the ignore list.
-
-//   A cloud project is created for you in the AWS Amplify Console that can be accessed by running amplify console. The Console provides a list of backend 
-//   environments, deep links to provisioned resources per Amplify category, status of recent deployments, and instructions on how to promote, 
-//   clone, pull, and delete backend resources.
-// */
-// /* REACT AND RN */
-// import { 
-//   StyleSheet, 
-//   Text, 
-//   View,
-//   Dimensions
-// } from 'react-native';
-// import React, { 
-//   useState, 
-//   useEffect, 
-//   useRef,
-//   useReducer
-// } from 'react';
-
-// import GridView from 'react-native-draggable-gridview'
-// import Workgrid  from './src/components/Workgrid.js';
-
-// // /* AWS AMPLIFY */
-// // import Amplify from 'aws-amplify';
-// // import { withAuthenticator } from 'aws-amplify-react-native'
-// // import { API, graphqlOperation } from '@aws-amplify/api';
-// // import { Auth } from '@aws-amplify/auth';
-// // import awsmobile from './aws-exports';
-// // Amplify.configure({
-// //   ...awsmobile,
-// //   Analytics: {
-// //     disabled: true,
-// //   },
-// //   graphql_headers: async () => {
-// //     const currentSession = await Auth.currentSession();
-// //     return { Authorization: currentSession.getIdToken().getJwtToken() };
-// //   }
-// // });
-
-// /* IMPORTED COMPONENTS */
-// // import { ListBooks, AddBook } from './src/components/graphql.js';
-// import RenderDraggable from './src/RenderDraggable.js';
-// // import { Tab } from 'react-native-elements';
-// // import AddBooks from './src/components/AddBook.js';
-
-// import {
-//   GridContext,
-//   gridReducer,
-// } from "./src/components/contexts/GridContext";
-// import {
-//   MotionContext,
-//   motionReducer,
-// } from "./src/components/contexts/MotionContext";
-
-// /* STYLES AND MISC */
-// let Window = Dimensions.get('window');
-// let styles = StyleSheet.create({
-//     mainContainer: {
-//         flex    : 1
-//     },
-//     book: {
-//       borderBottomWidth: 1,
-//       borderBottomColor: '#ddd',
-//       paddingVertical: 10
-//     }
-// });
-
-// function App() {
-//   // gather user resources
-//   const [data, setData] = useState(['1', '2', '3', '4', '5', '6'])
-//   // const [books, setBooks] = useState([]);
-//   const [whichView, setWhichView] = useState('CASE');
-//   // boardView built with resources and passed as children to main view
-//   const [boardView, setBoardView] = useState(null);
-//   // use context and reducer hooks to build a virtual rep of the grid state
-//   const [gridState, dispatch] = useReducer(
-//     gridReducer, 
-//     { 
-//       whichView: 'CASE',
-//       whichClick: '',
-//       grid: [
-//         {
-//           "id":1,
-//           "pos":1
-//         },
-//         {
-//           "id":2,
-//           "pos":2
-//         },
-//         {
-//           "id":3,
-//           "pos":3
-//         }
-//       ]
-//     }
-//   )
-
-//   const [motionState, dispatchMotion] = useReducer(
-//     motionReducer,
-//     {
-//       movingList: []
-//     }
-
-//   )
-
-//   // useEffect(() => {
-//   //   const getResult = async () => {
-//   //     const books = await API.graphql(graphqlOperation(ListBooks));
-//   //     /* get all attached resources here if I have to */
-//   //     // sort books so graphql doesn't have to
-//   //     const stateOut = books.data.listBooks.items.sort((a,b) => {
-//   //       return (a.position > b.position) ? 1 : -1
-//   //     })
-//   //     setBooks(stateOut);
-//   //   };
-
-//   //   try {
-//   //     getResult();
-//   //   } catch(err) {
-//   //     console.log(err)
-//   //   }
-//   // }, [])
-
-//   /*
-//     In second side effect, build the boardView based on the amount of books the user has saved.
-
-//     Initialize the height of the device window (static) as well as the width of what each block
-//     will be (arbitrarily 3, but this can be based on a user setting or the window width itself).
-//     Additionally, init 'tick' values for both the x and y axis (dx,dy).
-
-//     As the books are looped through, reconcile the tick values on each iteration. If i%3 is 
-//     bouncing to 0, this means we should start a new row (dx = 0, dy + 1). In any other event, 
-//     given that i%3 != 0, dx should tick up. Use the tick values to calculate the (arbitrary) top
-//     and (dynamic based on width) left sides of each square.    
-//   */
-
-//   useEffect(() => {
-//     const w = Window.width / 3;
-//     const h = Window.height;
-//     let dx = 0;
-//     let dy = 0; 
-//     // var virtualGridSquare = {};
-//     // virtualGridSquare['id'] = props.id;
-//     // virtualGridSquare['pos'] = props.position;
-//     // dispatch(buildGrid(virtualGridSquare))
-//     const newBoardView = [1,2,3].map((b,i) => {
-//                   if (i>0) {
-//                     dx += 1
-//                     if (i%3 === 0) {
-//                       dx = 0
-//                       dy += 1
-//                     }
-//                   }
-//                   const calcTop = (0+(dy*100))
-//                   const calcLeft = (0+(dx*w))
-//                   // console.log('book:', b)
-
-//                   // console.log('calcTop: ', calcTop);
-//                   // console.log('calcLeft: ', calcLeft);
-
-//                   const s = StyleSheet.create({
-//                     gridSquare: {
-//                       position: 'absolute',
-//                       borderColor: '#000000',
-//                       top: calcTop,
-//                       left: calcLeft,
-//                       width: w,
-//                       height: 100,
-//                       borderWidth: 1
-//                     }
-//                   });
-//                   return (
-//                     <View style={s.gridSquare} key={i+1}>
-//                       <RenderDraggable 
-//                         idx={i}
-//                         westBound={calcLeft}
-//                         northBound={calcTop}
-//                         eastBound={calcLeft+(w)}
-//                         southBound={calcTop+100}
-//                         width={Window.width}
-//                         height={Window.height}
-//                         row={dy+1}
-//                         column={dx+1}
-//                         id={b.id}
-//                         position={b.position}
-//                         // gridState={gridState}
-//                       />
-//                     </View>
-//                   )
-//                 });
-//     setBoardView(newBoardView)
-//   }, [])
-
-//   useEffect(() => {
-//     const w = Window.width / 3;
-//     const h = Window.height;
-//     let dx = 0;
-//     let dy = 0; 
-//     switch (gridState.whichView) {
-//       case "CASE":
-//         console.log("case view in effect")
-//         const baseView = [1,2,3].map((b,i) => {
-//                 if (i>0) {
-//                   dx += 1
-//                   if (i%3 === 0) {
-//                     dx = 0
-//                     dy += 1
-//                   }
-//                 }
-//                 const calcTop = (0+(dy*100))
-//                 const calcLeft = (0+(dx*w))
-//                 // console.log('book:', b)
-
-//                 // console.log('calcTop: ', calcTop);
-//                 // console.log('calcLeft: ', calcLeft);
-
-//                 const s = StyleSheet.create({
-//                   gridSquare: {
-//                     position: 'absolute',
-//                     borderColor: '#000000',
-//                     top: calcTop,
-//                     left: calcLeft,
-//                     width: w,
-//                     height: 100,
-//                     borderWidth: 1
-//                   }
-//                 });
-//                 return (
-//                   <View style={s.gridSquare} key={i+1}>
-//                     <RenderDraggable 
-//                       idx={i}
-//                       westBound={calcLeft}
-//                       northBound={calcTop}
-//                       eastBound={calcLeft+(w)}
-//                       southBound={calcTop+100}
-//                       width={Window.width}
-//                       height={Window.height}
-//                       row={dy+1}
-//                       column={dx+1}
-//                       id={b}
-//                       position={b}
-//                       // gridState={gridState}
-//                     />
-//                   </View>
-//                 )
-//               });
-//         setBoardView(baseView)
-//         return;
-//     //   case "DEPO":
-//     //     const idx = [1,2,3].findIndex(b => b['id'] === gridState.whichClick);
-//     //     const secondView = books[idx].depos.items.map((b,i) => {
-//     //                       if (i>0) {
-//     //               dx += 1
-//     //               if (i%3 === 0) {
-//     //                 dx = 0
-//     //                 dy += 1
-//     //               }
-//     //             }
-//     //             const calcTop = (0+(dy*100))
-//     //             const calcLeft = (0+(dx*w))
-//     //             // console.log('book:', b)
-
-//     //             // console.log('calcTop: ', calcTop);
-//     //             // console.log('calcLeft: ', calcLeft);
-
-//     //             const s = StyleSheet.create({
-//     //               gridSquare: {
-//     //                 position: 'absolute',
-//     //                 borderColor: '#000000',
-//     //                 top: calcTop,
-//     //                 left: calcLeft,
-//     //                 width: w,
-//     //                 height: 100,
-//     //                 borderWidth: 1
-//     //               }
-//     //             });
-//     //             return (
-//     //               <View style={s.gridSquare} key={i+1}>
-//     //                 <RenderDraggable 
-//     //                   idx={i}
-//     //                   westBound={calcLeft}
-//     //                   northBound={calcTop}
-//     //                   eastBound={calcLeft+(w)}
-//     //                   southBound={calcTop+100}
-//     //                   width={Window.width}
-//     //                   height={Window.height}
-//     //                   row={dy+1}
-//     //                   column={dx+1}
-//     //                   id={b.id}
-//     //                   position={b.position}
-//     //                   // gridState={gridState}
-//     //                 />
-//     //               </View>
-//     //             )
-//     //     });
-//     //     setBoardView(secondView)
-//     //     return;
-//     //   default:
-//     //     return;
-//     }
-//   }, [gridState.whichView])
-
-  
-
-//   return (
-//       <GridView
-//           data={data}
-//           numColumns={3}
-//           renderItem={(item) => (
-//               <View style={{ flex: 1, margin: 1, justifyContent: 'center', backgroundColor: 'gray' }}>
-//               <Text style={{ textAlign: 'center' }}>{item}</Text>
-//               </View>
-//           )}
-//           onReleaseCell={(items) => setData(items)}
-//       />
-//     // <MotionContext.Provider value={[motionState, dispatchMotion]}>
-//     //   <GridContext.Provider value={[gridState, dispatch]}>    
-//     //   </GridContext.Provider>
-//     // </MotionContext.Provider>
-//   );
-// }
-
-// export default App;
